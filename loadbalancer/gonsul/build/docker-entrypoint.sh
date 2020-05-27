eeac222ce4fa44f667dfe4af5d5eadd7155b273f
Bradley Barrett <bradleybarrett@users.noreply.github.com>
2020-05-27 17:18:34 -0400

    Update comments in gonsul build files

--- loadbalancer/gonsul/build/docker-entrypoint.sh
+++ loadbalancer/gonsul/build/docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 user=gonsul
 home=/home/${user}
 
-# Copy .ssh files from bind-mount to user's home directory. 
-# This allows permission changes and prevents propagating files changes to the host.
+# Copy .ssh files from bind-mount to the user's home directory. 
+# Permissions and ownership of the copy can be modified without propagating changes to the host.
 cp -R /tmp/.ssh ${home}/.ssh
 
 # Give the user ownership of the .ssh files in their home directory.
@@ -13,6 +13,7 @@ chown -R ${user}:${user} ${home}/.ssh
 
 # If the first arg looks like a flag, assume we want to run gonsul server.
 if [ "${1:0:1}" = '-' ]; then
+    # Prefix the input command args with the gonsul command.
     set -- gonsul "$@"
 fi
 
